2abbc9de3f42a3d7ce41bcf8b6fe03de6e7e99b7
theengineerwhosengihere <connie.zhao2@education.nsw.gov.au>
2025-12-02 23:34:00 +0000

    finished PWA

---
+++ myPWA/.database/myQuery.sql
@@ -0,0 +1,2 @@
+CREATE TABLE extension(extID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, hyperlink TEXT NOT NULL,about TEXT NOT NULL,image TEXT NOT NULL,language TEXT NOT NULL);
+INSERT INTO extension(extID,name,hyperlink,about,image,language) VALUES (1,"Brioche","public/brioche.html","Brioche","public/images/bread/brioche.png","HTML"); 
